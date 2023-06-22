5e52895f5a3c8b34eb4f90805c5148e987c94079
bukhamm <69335357+bukhamm@users.noreply.github.com>
2023-06-22 03:56:49 -0400

    Se crea el Instalador

---
+++ Instalador Yakuza Kieami 2/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Instalador_Yakuza_Kieami_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new System.Windows.Forms.TextBox();
+            button1 = new System.Windows.Forms.Button();
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            radioButton2 = new System.Windows.Forms.RadioButton();
+            radioButton1 = new System.Windows.Forms.RadioButton();
+            progressBar1 = new System.Windows.Forms.ProgressBar();
+            label1 = new System.Windows.Forms.Label();
+            install = new System.Windows.Forms.Button();
+            label2 = new System.Windows.Forms.Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(179, 47);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(434, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(619, 47);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(25, 23);
+            button1.TabIndex = 1;
+            button1.Text = "...";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new System.Drawing.Point(64, 212);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new System.Drawing.Size(200, 76);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Seleccione su Version del Juego";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new System.Drawing.Point(6, 47);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new System.Drawing.Size(82, 19);
+            radioButton2.TabIndex = 4;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Gamepass";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new System.Drawing.Point(6, 22);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new System.Drawing.Size(61, 19);
+            radioButton1.TabIndex = 3;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Steam";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new System.Drawing.Point(179, 93);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new System.Drawing.Size(434, 40);
+            progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(179, 20);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(0, 15);
+            label1.TabIndex = 4;
+            // 
+            // install
+            // 
+            install.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            install.Location = new System.Drawing.Point(532, 212);
+            install.Name = "install";
+            install.Size = new System.Drawing.Size(178, 76);
+            install.TabIndex = 5;
+            install.Text = "Instalar Traduccion";
+            install.UseVisualStyleBackColor = true;
+            install.Click += install_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Castellar", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label2.Location = new System.Drawing.Point(179, 147);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(0, 14);
+            label2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 331);
+            Controls.Add(label2);
+            Controls.Add(install);
+            Controls.Add(label1);
+            Controls.Add(progressBar1);
+            Controls.Add(groupBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Asistente de Instalacion de Yakuza Kiwami 2";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button install;
+        private System.Windows.Forms.Label label2;
+    }
+}
